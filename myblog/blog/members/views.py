dfd862195483619425a74242f9bed37c9799ce5d
Dmytro Lytvynenko <105833335+DmytroLyt@users.noreply.github.com>
2022-08-27 21:44:56 -0500

    Update views.py

--- myblog/blog/members/views.py
+++ myblog/blog/members/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SingUpForm, EditProfileForm, PasswordChange
+from myblog.models import User_Profile
+
+class EditProfilePageView(generic.UpdateView):
+    model = User_Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'instagram_url', 'github_url', 'linkedin_url']
+    success_url = reverse_lazy('home')
+
+
+class ProfilePageView(DetailView):
+    model = User_Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        user_page = User_Profile.objects.all()
+        context = super(ProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(User_Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChange
