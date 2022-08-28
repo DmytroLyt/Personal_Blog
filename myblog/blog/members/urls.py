615d71259ca9a62b3b432bdada339043f4c4c615
Dmytro Lytvynenko <105833335+DmytroLyt@users.noreply.github.com>
2022-08-27 21:45:41 -0500

    Update urls.py

--- myblog/blog/members/urls.py
+++ myblog/blog/members/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import UserRegister, UserEdit, PasswordsChangeView
+from .views import UserRegister, UserEdit, PasswordsChangeView, ProfilePageView, EditProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/', UserRegister.as_view(), name='register'),
     path('edit_profile/', UserEdit.as_view(), name='edit_profile'),
-    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
+    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('password_changed', views.password_changed, name="password_changed"),
+    path('<int:pk>/user_profile/', ProfilePageView.as_view(), name='profile_page'),
+    path('<int:pk>/edit_profile_page', EditProfilePageView.as_view(), name='edit_profile_page'),
+
 ]
