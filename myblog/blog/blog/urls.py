99813ba845caa541ba175a4f369d382db9ea28c4
Dmytro Lytvynenko <105833335+DmytroLyt@users.noreply.github.com>
2022-09-11 13:40:41 -0500

    Update urls.py

--- myblog/blog/blog/urls.py
+++ myblog/blog/blog/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myblog.urls')),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('myblog.urls')),
+                  path('members/', include('django.contrib.auth.urls')),
+                  path('members/', include('members.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
